ffc2e044a832752e5a92212611d612364473a1bc
Thomas Henningson <thenn002@ucr.edu>
2020-05-28 02:12:36 -0700

    completed part 5

--- turnin/thenn002_lab11_part2.c
+++ turnin/thenn002_lab11_part2.c
@@ -21,40 +21,6 @@
 #include "seven_seg.h"
 #include "stack.h"
 
-unsigned char input, data;
-
-enum KeypadStates{K_Wait, K_Retrieve};
-int KeypadSMTick(int state) {
-	switch(state) {
-		case K_Wait:
-			if(input == '\0') {
-				state = K_Wait;
-			}
-			else if(input != '\0') {
-				state = K_Retrieve;
-			}
-			break;
-		case K_Retrieve:
-			state = K_Wait;
-			break;
-		default:
-			state = K_Wait;
-			input = '\0';
-			break;
-	}
-	switch(state) {
-		case K_Wait:
-			input = GetKeypadKey();
-			break;
-		case K_Retrieve:
-			data = input;
-			input = '\0';
-			break;
-		default: break;
-	}
-	return state;
-}
-
 const unsigned char phrase[] = "CS120B is Legend... wait for it DARY!";
 unsigned char column, num;
 
@@ -99,21 +65,16 @@ int main(void){
         DDRB = 0xFF; PORTB = 0x00;
         DDRD = 0xFF; PORTD = 0x00;	
 
-	static task task1, task2;
-	task *tasks[] = { &task1, &task2 };
-	const unsigned short numTasks = 2;
+	static task task1;
+	task *tasks[] = { &task1 };
+	const unsigned short numTasks = 1;
 
 	task1.state = -1;
-	task1.period = 50;
+	task1.period = 300;
 	task1.elapsedTime = task1.period;
-	task1.TickFct = &KeypadSMTick;
+	task1.TickFct = &DisplayPhraseSMTick;
 
-	task2.state = -1;
-        task2.period = 300;
-        task2.elapsedTime = task2.period;
-        task2.TickFct = &DisplayPhraseSMTick;
-
-	TimerSet(50);
+	TimerSet(300);
 	TimerOn();
         LCD_init();
         LCD_ClearScreen();
@@ -126,7 +87,7 @@ int main(void){
 				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
 				tasks[i]->elapsedTime = 0;
 			}
-			tasks[i]->elapsedTime += 50;
+			tasks[i]->elapsedTime += 300;
 		}
 		while (!TimerFlag);
 		TimerFlag = 0;
